393c685073de1708c47191724a9375bbe133a647
David Nolen <david.nolen@gmail.com>
2013-04-13 19:30:30 -0400

    add fib-machine

--- chap5_2_4.scm
+++ chap5_2_4.scm
@@ -403,7 +403,38 @@
   (get-register-contents fact-machine 'val))
 
 (define fib-machine
-  '())
+  (make-machine
+    '(n val continue)
+    (list (list '+ +) (list '- -) (list '< <))
+    '(  (assign continue (label fib-done))
+      fib-loop
+        (test (op <) (reg n) (const 2))
+        (branch (label immediate-answer))
+        (save continue)
+        (assign continue (label afterfib-n-1))
+        (save n)
+        (assign n (op -) (reg n) (const 1))
+        (goto (label fib-loop))
+      afterfib-n-1
+        (restore n)
+        (restore continue)
+        (assign n (op -) (reg n) (const 2))
+        (save continue)
+        (assign continue (label after-fib-n-2))
+        (save val)
+        (goto (label fib-loop))
+      afterfib-n-2
+        (assign n (reg val))
+        (restore val)
+        (restore continue)
+        (assign val (op +) (reg val) (reg n))
+        (goto (reg continue))
+      immediate-answer
+        (assign val (reg n))
+        (goto (reg continue))
+      fib-done)))
 
 (define (run-fib-machine)
-  '())
+  (set-register-contents! fib-machine 'n 5)
+  (start fib-machine)
+  (get-register-contents fib-machine 'val))
