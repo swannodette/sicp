82aa6cf1e531800b44e84a377045a2bb96d2571a
David Nolen <david.nolen@gmail.com>
2013-04-13 19:24:47 -0400

    can run the fact-machine

--- chap5_2_4.scm
+++ chap5_2_4.scm
@@ -278,7 +278,7 @@
   (let ((reg (get-register machine
                (stack-inst-reg-name inst))))
     (lambda ()
-      (push stack (get-contents))
+      (push stack (get-contents reg))
       (advance-pc pc))))
 
 (define (make-restore inst machine stack pc)
